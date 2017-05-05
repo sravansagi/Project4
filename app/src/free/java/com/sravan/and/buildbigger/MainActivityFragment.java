eca47d4e9795ecfde43dd903f8e6ba3b94c56d9d
Sravan <venkatasravan13@gmail.com>
2017-05-05 11:57:04 +0530

    Added InterstitalAd for free flavor

--- app/src/free/java/com/sravan/and/buildbigger/MainActivityFragment.java
+++ app/src/free/java/com/sravan/and/buildbigger/MainActivityFragment.java
@@ -9,11 +9,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
 
+import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.sravan.and.jokeviewlib.MainActivityJokeView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -24,34 +27,87 @@ public class MainActivityFragment extends Fragment implements EndpointsAsyncTask
     Button buttonJoke;
     @BindView(R.id.joke_progressbar)
     ProgressBar jokeProgress;
+    InterstitialAd mInterstitialAd;
+    String joke;
+    boolean displayInterstitalAd;
+    boolean getJokeTaskComplete;
+
     public MainActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        joke = "";
+        displayInterstitalAd = false;
+        getJokeTaskComplete = false;
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         ButterKnife.bind(this,rootView);
         buttonJoke.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 getJoke();
+                if (mInterstitialAd.isLoaded()) {
+                    displayInterstitalAd = true;
+                    mInterstitialAd.show();
+                } else {
+                    jokeProgress.setVisibility(View.VISIBLE);
+                }
             }
         });
 
+        AdView mAdView = (AdView) rootView.findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("6C21B0458E3BA016232450AFD157E6C0")
+                .build();
+        mAdView.loadAd(adRequest);
+        mInterstitialAd = new InterstitialAd(getActivity());
+        mInterstitialAd.setAdUnitId(getResources().getString(R.string.interstitial_ad_unit_id));
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                displayInterstitalAd = false;
+                requestNewInterstitial();
+                if (getJokeTaskComplete){
+                    getJokeTaskComplete = false;
+                    launchActivity(joke);
+                } else {
+                    jokeProgress.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        requestNewInterstitial();
         return rootView;
     }
 
     private void getJoke() {
-        jokeProgress.setVisibility(View.VISIBLE);
+        //jokeProgress.setVisibility(View.VISIBLE);
         new EndpointsAsyncTask(this).execute(getActivity());
     }
 
     @Override
     public void done(String result) {
         jokeProgress.setVisibility(View.GONE);
+        if(!displayInterstitalAd){
+            launchActivity(result);
+        } else{
+            getJokeTaskComplete = true;
+            joke = result;
+        }
+    }
+
+    private void launchActivity(String result){
         Intent intent = new Intent(getActivity(), MainActivityJokeView.class);
-        intent.putExtra(Intent.EXTRA_TEXT, result );
+        intent.putExtra(Intent.EXTRA_TEXT, result);
         startActivity(intent);
+    }
+
+    private void requestNewInterstitial() {
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("6C21B0458E3BA016232450AFD157E6C0")
+                .build();
+        mInterstitialAd.loadAd(adRequest);
     }
 }
